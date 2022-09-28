3b0c155cf6ec6bce9a2c9155ea2495a8db320a26
cristianenajera@gmail.com <*O+0clqH5dR@]mC]{1w!|6:P]&/.XFo}Mp0HtAlAQ&~Q:@{a(^iVn'H;jggA)!y!Gcff5\lc>
2022-09-28 16:42:31 -0700

    Query db

--- app.py
+++ app.py
@@ -10,19 +10,14 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_PORT = os.getenv("DB_PORT")
 DB_USERNAME = os.getenv("DB_USERNAME")
 
-engine = create_engine(f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+print(f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    df = pd.read_sql(
-        "select * from public.test",
-        con=engine
-    )
-    names = ', '.join(df['name'].values.tolist())
-    return f'We have this names {names} in the database'
+    return f'After pipeline'
 
 
 if __name__ == '__main__':
