9b15700a6a4cde7f5c312f844bd08b7a12fce744
Cristian Najera <cristianenajera@gmail.com>
2022-10-05 21:27:06 -0700

    modified buildspec

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
-import os
 from flask import Flask
-import pandas as pd
-from sqlalchemy import create_engine
-
-
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
-DB_USERNAME = os.getenv("DB_USERNAME")
-
-engine = create_engine(f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    df = pd.read_sql(
-        "select * from public.test",
-        con=engine
-    )
-    shape = df.shape
-    rows, cols = shape[0], shape[1]
-    names = ', '.join(df['name'].values.tolist())
-    return f'We have {rows} names, and there are: {names}.'
+    return f'<h1>Test</h1>'
 
 
 if __name__ == '__main__':
